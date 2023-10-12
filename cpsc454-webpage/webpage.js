a35ecdadb44fa2afe7ee8230e401252dae52f6e1
ginnaaleee <131627008+ginnaaleee@users.noreply.github.com>
2023-10-12 10:16:19 -0700

    Update webpage.js

--- cpsc454-webpage/webpage.js
+++ cpsc454-webpage/webpage.js
@@ -1,15 +1,15 @@
-
-  document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     let links = document.querySelectorAll('nav ul li a');
 
     links.forEach(link => {
       link.addEventListener('click', function(e) {
-        e.preventDefault(); 
+        e.preventDefault();
         
         let targetId = this.getAttribute('href').substring(1); 
         let targetElement = document.getElementById(targetId); 
         
-        targetElement.scrollIntoView({ behavior: 'smooth' }); 
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+
       });
     });
   });
